47541e80851f710a9ab77c58dd766d4fee8caed9
Markus <markus.gross@hm.edu>
2021-08-16 15:41:52 +0200

    minor changes

--- manydepth/inference.py
+++ manydepth/inference.py
@@ -1,9 +1,3 @@
-# Copyright Niantic 2021. Patent Pending. All rights reserved.
-#
-# This software is licensed under the terms of the ManyDepth licence
-# which allows for non-commercial use only, the full terms of which are made
-# available in the LICENSE file.
-
 import os
 import json
 import argparse
@@ -19,26 +13,6 @@ from manydepth import networks
 from .layers import transformation_from_parameters
 
 import time
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Simple testing funtion for ManyDepth models.')
-
-    parser.add_argument('--target_image_path', type=str,
-                        help='path to a test image to predict for', required=True)
-    parser.add_argument('--source_image_path', type=str,
-                        help='path to a previous image in the video sequence', required=True)
-    parser.add_argument('--intrinsics_json_path', type=str,
-                        help='path to a json file containing a normalised 3x3 intrinsics matrix',
-                        required=True)
-    parser.add_argument('--model_path', type=str,
-                        help='path to a folder of weights to load', required=True)
-    parser.add_argument('--mode', type=str, default='multi', choices=('multi', 'mono'),
-                        help='"multi" or "mono". If set to "mono" then the network is run without '
-                             'the source image, e.g. as described in Table 5 of the paper.',
-                        required=False)
-    return parser.parse_args()
-
 
 def load_and_preprocess_image(image_path, resize_width, resize_height):
     image = pil.open(image_path).convert('RGB')
@@ -67,131 +41,121 @@ def load_and_preprocess_intrinsics(intrinsics_path, resize_width, resize_height)
         return K.cuda(), invK.cuda()
     return K, invK
 
+# Select GPU or CPU
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print("-> Loading model from ", args.model_path)
 
-def test_simple(args):
-    """Function to predict for a single image or folder of images
-    """
-    assert args.model_path is not None, \
-        "You must specify the --model_path parameter"
+# Loading pretrained model
+print("   Loading pretrained encoder")
+encoder_dict = torch.load(os.path.join(args.model_path, "encoder.pth"), map_location=device)
+encoder = networks.ResnetEncoderMatching(18, False,
+                                            input_width=encoder_dict['width'],
+                                            input_height=encoder_dict['height'],
+                                            adaptive_bins=True,
+                                            min_depth_bin=encoder_dict['min_depth_bin'],
+                                            max_depth_bin=encoder_dict['max_depth_bin'],
+                                            depth_binning='linear',
+                                            num_depth_bins=96)
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    print("-> Loading model from ", args.model_path)
+filtered_dict_enc = {k: v for k, v in encoder_dict.items() if k in encoder.state_dict()}
+encoder.load_state_dict(filtered_dict_enc)
 
-    # Loading pretrained model
-    print("   Loading pretrained encoder")
-    encoder_dict = torch.load(os.path.join(args.model_path, "encoder.pth"), map_location=device)
-    encoder = networks.ResnetEncoderMatching(18, False,
-                                             input_width=encoder_dict['width'],
-                                             input_height=encoder_dict['height'],
-                                             adaptive_bins=True,
-                                             min_depth_bin=encoder_dict['min_depth_bin'],
-                                             max_depth_bin=encoder_dict['max_depth_bin'],
-                                             depth_binning='linear',
-                                             num_depth_bins=96)
+print("   Loading pretrained decoder")
+depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc, scales=range(4))
 
-    filtered_dict_enc = {k: v for k, v in encoder_dict.items() if k in encoder.state_dict()}
-    encoder.load_state_dict(filtered_dict_enc)
+loaded_dict = torch.load(os.path.join(args.model_path, "depth.pth"), map_location=device)
+depth_decoder.load_state_dict(loaded_dict)
 
-    print("   Loading pretrained decoder")
-    depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc, scales=range(4))
+print("   Loading pose network")
+pose_enc_dict = torch.load(os.path.join(args.model_path, "pose_encoder.pth"),
+                            map_location=device)
+pose_dec_dict = torch.load(os.path.join(args.model_path, "pose.pth"), map_location=device)
 
-    loaded_dict = torch.load(os.path.join(args.model_path, "depth.pth"), map_location=device)
-    depth_decoder.load_state_dict(loaded_dict)
+pose_enc = networks.ResnetEncoder(18, False, num_input_images=2)
+pose_dec = networks.PoseDecoder(pose_enc.num_ch_enc, num_input_features=1,
+                                num_frames_to_predict_for=2)
 
-    print("   Loading pose network")
-    pose_enc_dict = torch.load(os.path.join(args.model_path, "pose_encoder.pth"),
-                               map_location=device)
-    pose_dec_dict = torch.load(os.path.join(args.model_path, "pose.pth"), map_location=device)
+pose_enc.load_state_dict(pose_enc_dict, strict=True)
+pose_dec.load_state_dict(pose_dec_dict, strict=True)
 
-    pose_enc = networks.ResnetEncoder(18, False, num_input_images=2)
-    pose_dec = networks.PoseDecoder(pose_enc.num_ch_enc, num_input_features=1,
-                                    num_frames_to_predict_for=2)
+# Setting states of networks
+encoder.eval()
+depth_decoder.eval()
+pose_enc.eval()
+pose_dec.eval()
+if torch.cuda.is_available():
+    encoder.cuda()
+    depth_decoder.cuda()
+    pose_enc.cuda()
+    pose_dec.cuda()
 
-    pose_enc.load_state_dict(pose_enc_dict, strict=True)
-    pose_dec.load_state_dict(pose_dec_dict, strict=True)
+# inference time params
+inf_time_warmup_iter = 2 # change number of warmup iterations
+inf_time_calc_iter = 2 # change number of itterations that are used to calculate average inference time
+inf_time_sum = 0
 
-    # Setting states of networks
-    encoder.eval()
-    depth_decoder.eval()
-    pose_enc.eval()
-    pose_dec.eval()
-    if torch.cuda.is_available():
-        encoder.cuda()
-        depth_decoder.cuda()
-        pose_enc.cuda()
-        pose_dec.cuda()
+for i in range(inf_time_warmup_iter + inf_time_calc_iter):
+
+    # info log
+    if i == 0:
+        print('starting warmup')
+    elif i == inf_time_warmup_iter:
+        print('starting to average inference time')
+    print('iteration',i)
     
-    # inference time params
-    inf_time_sum = 0
-    inf_time_warmup_iter = 50
-    inf_time_calc_iter = 50
+    # start inference time measurement
+    if i >= inf_time_warmup_iter:
+        inf_time_start = time.time()
+    
 
-    for i in range(inf_time_warmup_iter + inf_time_calc_iter):
+    # Load input data
+    input_image, original_size = load_and_preprocess_image(args.target_image_path,
+                                                        resize_width=encoder_dict['width'],
+                                                        resize_height=encoder_dict['height'])
 
-        # info log
-        if i == 0:
-            print('starting warmup')
-        elif i == inf_time_warmup_iter:
-            print('starting to average inference time')
-        print('iteration',i)
-        
-        # start inference time measurement
-        if i >= inf_time_warmup_iter:
-            inf_time_start = time.time()
-        
-
-        # Load input data
-        input_image, original_size = load_and_preprocess_image(args.target_image_path,
-                                                            resize_width=encoder_dict['width'],
-                                                            resize_height=encoder_dict['height'])
-
-        source_image, _ = load_and_preprocess_image(args.source_image_path,
-                                                    resize_width=encoder_dict['width'],
-                                                    resize_height=encoder_dict['height'])
-
-        K, invK = load_and_preprocess_intrinsics(args.intrinsics_json_path,
+    source_image, _ = load_and_preprocess_image(args.source_image_path,
                                                 resize_width=encoder_dict['width'],
                                                 resize_height=encoder_dict['height'])
 
-        with torch.no_grad():
+    K, invK = load_and_preprocess_intrinsics(args.intrinsics_json_path,
+                                            resize_width=encoder_dict['width'],
+                                            resize_height=encoder_dict['height'])
 
-            # Estimate poses
-            pose_inputs = [source_image, input_image]
-            pose_inputs = [pose_enc(torch.cat(pose_inputs, 1))]
-            axisangle, translation = pose_dec(pose_inputs)
-            pose = transformation_from_parameters(axisangle[:, 0], translation[:, 0], invert=True)
+    with torch.no_grad():
 
-            if args.mode == 'mono':
-                pose *= 0  # zero poses are a signal to the encoder not to construct a cost volume
-                source_image *= 0
+        # Estimate poses
+        pose_inputs = [source_image, input_image]
+        pose_inputs = [pose_enc(torch.cat(pose_inputs, 1))]
+        axisangle, translation = pose_dec(pose_inputs)
+        pose = transformation_from_parameters(axisangle[:, 0], translation[:, 0], invert=True)
 
-            # Estimate depth
-            output, lowest_cost, _ = encoder(current_image=input_image,
-                                            lookup_images=source_image.unsqueeze(1),
-                                            poses=pose.unsqueeze(1),
-                                            K=K,
-                                            invK=invK,
-                                            min_depth_bin=encoder_dict['min_depth_bin'],
-                                            max_depth_bin=encoder_dict['max_depth_bin'])
+        if args.mode == 'mono':
+            pose *= 0  # zero poses are a signal to the encoder not to construct a cost volume
+            source_image *= 0
 
-            output = depth_decoder(output)
+        # Estimate depth
+        output, lowest_cost, _ = encoder(current_image=input_image,
+                                        lookup_images=source_image.unsqueeze(1),
+                                        poses=pose.unsqueeze(1),
+                                        K=K,
+                                        invK=invK,
+                                        min_depth_bin=encoder_dict['min_depth_bin'],
+                                        max_depth_bin=encoder_dict['max_depth_bin'])
 
-            sigmoid_output = output[("disp", 0)]
-            sigmoid_output_resized = torch.nn.functional.interpolate(
-                sigmoid_output, original_size, mode="bilinear", align_corners=False)
-            sigmoid_output_resized = sigmoid_output_resized.cpu().numpy()[:, 0]
+        output = depth_decoder(output)
 
-        # calculate inference time
-        if i >= inf_time_warmup_iter:
-            inf_time_end = time.time()
-            inf_time = (inf_time_end - inf_time_start) * 1000 # time in ms
-            inf_time_sum += inf_time
-    
-    # average inference time
-    inf_time_avg = inf_time_sum / inf_time_calc_iter
+        sigmoid_output = output[("disp", 0)]
+        sigmoid_output_resized = torch.nn.functional.interpolate(
+            sigmoid_output, original_size, mode="bilinear", align_corners=False)
+        sigmoid_output_resized = sigmoid_output_resized.cpu().numpy()[:, 0]
 
-    print('average inference time after {} iterations: {} ms'.format(inf_time_calc_iter, int(inf_time_avg)))
-        
-if __name__ == '__main__':
-    args = parse_args()
-    test_simple(args)
+    # calculate inference time
+    if i >= inf_time_warmup_iter:
+        inf_time_end = time.time()
+        inf_time = (inf_time_end - inf_time_start) * 1000 # time in ms
+        inf_time_sum += inf_time
+
+# average inference time
+inf_time_avg = inf_time_sum / inf_time_calc_iter
+
+print('average inference time after {} iterations: {} ms'.format(inf_time_calc_iter, int(inf_time_avg)))
